fd93a21b766871760d2a5e9f91e9f7e38e94ba8b
elle510 <elle0510@gmail.com>
2018-01-02 18:04:00 +0900

    login

--- note/note/settings.py
+++ note/note/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'note.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates'), os.path.join(BASE_DIR, 'memo/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,3 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# LOGIN_URL = '/accounts/login/'
+# LOGOUT_URL = '/accounts/logout'
+LOGIN_REDIRECT_URL = '/'
