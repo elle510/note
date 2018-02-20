9a900ee39a36ebd76134798d42975de19e55404e
elle510 <elle0510@gmail.com>
2018-02-20 23:03:36 +0900

    login

--- note/authentication/urls.py
+++ note/authentication/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 
 from . import views
 
@@ -6,4 +7,5 @@ urlpatterns = [
     # url(r'^$', views.signin),
     url(r'^join/$', views.signup, name='join'),
     url(r'^login/$', views.signin, name='login'),
+    url(r'^logout/$', auth_views.logout, {'next_page' : '/'}, name='logout'), #로그아웃 후 홈으로 이동
 ]
